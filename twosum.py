1a63df65720db665a39382d6985fee58ef8d4a3a
nghianguyend <nghia.dn140206@gmail.com>
2025-11-08 14:29:44 +0100

    TwoSum

---
+++ twosum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)) :
+                if nums[i] + nums[j] == target :
+                    return [i, j]
+        return []
+                
+if __name__ == "__main__" :
+    nums = [2, 7, 11, 2, 4, 1]
+    target = 12
+    sol = Solution()
+    result = sol.twoSum(nums, target)
+    print("Result:", result)
